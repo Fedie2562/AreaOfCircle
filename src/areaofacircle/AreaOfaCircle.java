6bf73338aa6f5008a1573f5678f995b3ac474fe8
fedie2562 <fedie2562@7LLN6Y1.Student.UGDSB.ED>
2018-03-02 13:56:12 -0500

    Area of a Circle program

---
+++ src/areaofacircle/AreaOfaCircle.java
@@ -0,0 +1,16 @@
+/*
+ * Area of a Circle
+ */
+
+package areaofacircle;
+
+/**
+ *
+ * @author fedie2562
+ */
+public class AreaOfaCircle {
+    public static void main(String[] args) {
+        System.out.println("The area of a circle with a radius of 15cm is " + 3.14*15*15 + "cm squared");
+    }
+    
+}
